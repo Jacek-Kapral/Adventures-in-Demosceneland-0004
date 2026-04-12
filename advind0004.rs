3e703bf8e2b1f94433f3cf6a78884abde3ef7e0d
Jacek-Kapral <jacek.kapral@proton.me>
2026-04-12 22:50:29 +0200

    Add input from user to end program

--- advind0004.rs
+++ advind0004.rs
@@ -1,3 +1,8 @@
+use std::io::{self, Write};
 fn main() {
     println!("Hello, world!");
+    print!("Naciśnij Enter, aby zakończyć...");
+    let _ = io::stdout().flush();
+    let mut buf = String::new();
+    let _ = io::stdin().read_line(&mut buf);
 }
